6614c1e0383083fa22ff99d663ac0d6b41a5f8ff
ZED <khactuan2000yahoo@gmail.com>
2020-04-18 11:12:29 +0700

    btvn

---
+++ BTVN.py
@@ -0,0 +1,28 @@
+import math
+print(math.pi)      # in ra 3.141592653589793
+print(math.e)       #in ra 2.718281828459045
+print(math.floor(1.9))
+'''
+in ra 1
+floor là hàm trả về 1 số nguyên lớn nhất nhỏ hơn hoặc bằng số nhập vào
+'''
+print(math.ceil(2.1))
+'''
+in ra 3
+ceil là hàm trả về 1 số nguyên nhỏ nhất lớn hơn hoặc bằng số nhập vào 
+'''
+print(math.trunc(-2.9))     
+'''
+in ra -2
+trunc là hàm lấy phần nguyên của x
+'''
+print(math.factorial(5))
+'''
+in ra 120 
+factorial là hàm tính giai thừa của a 
+'''
+print(math.gcd(12, 18))
+'''
+in ra 6 
+gcd là hàm tìm ước số chung lớn nhất của a và b 
+'''
